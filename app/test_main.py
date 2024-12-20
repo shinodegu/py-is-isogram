ce2f9c1a919bd554358a05d2d078cfcfe1abc024
Ihor <andreiunderwhat@mail.com>
2024-12-20 21:30:44 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,34 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+class TestIsogram:
+    @pytest.mark.parametrize(
+        "word, result",
+        [
+            pytest.param(
+                "pineapple",
+                False,
+                id="test with non isogram"
+            ),
+            pytest.param(
+                "mad",
+                True,
+                id="test with isogram"
+            ),
+            pytest.param(
+                "HuGe",
+                True,
+                id="test with uppercase"
+            ),
+            pytest.param(
+                " ",
+                True,
+                id="test with empty str"
+            )
+        ]
+    )
+    def test_isogram(
+            self,
+            word: str,
+            result: bool) -> None:
+        assert is_isogram(word) == result
